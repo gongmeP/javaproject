b847edbe9b75212e75511fb9f14ecc061c01694a
Gongme <k102kg12@naver.com>
2023-04-14 12:23:01 +0900

    master

--- java2/src/project1/Object3.java
+++ java2/src/project1/Object3.java
@@ -3,33 +3,45 @@ package project1;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Object3 {
+public class Object3 extends ob2{
 
 	//원시배열 값을 Object 배열로 이관하기
 	public static void main(String[] args) {
 		
-		ob1 ob = new ob1();
+		ob2 ob = new ob2();
 		ob.data1();
 
 	}
 
 }
-class ob1{
-	ArrayList<Object> al; // 2차 배열 이상
-	Object data[][]; // 2원시 2차 배열
+class ob2{
+	ArrayList<Object[]> al2; // 2차 배열 이상
+	Object data2[][]; // 2원시 2차 배열
 	//2차 원시 배열 생성시 주의 사항은 Object를 적용 후 사용해야함
 	
 	public void data1() {
 		
-		this.data = new Object[][] {
+		this.data2 = new Object[][] {
 			{"hong","kim","park","hong","kang","jang"},
 			{2000,5000,4000,2200,3100,4120}
 		};
 		
-		this.al = new ArrayList<>(Arrays.asList(this.data));
-		System.out.println(Arrays.toString(this.al.get(0)));
+		this.al2 = new ArrayList<>(Arrays.asList(this.data2));
+		System.out.println(this.al2.get(1)[1]);
+		System.out.println(Arrays.toString(this.al2.get(1)));
 		
 		//해당 배열값 중 평균 총 포인트는 얼마인지 결과값 출력
 		//평균 포인트는 : 0원 입니다.
+		
+		int ea = this.al2.get(1).length;
+		
+		int x= 0;
+		Integer sum =0;
+		while(x < ea) {
+			sum += (int)this.al2.get(1)[x]; 
+			x++;
+		}
+		System.out.println(sum);
+		System.out.println(sum/ea);
 	}
 }
