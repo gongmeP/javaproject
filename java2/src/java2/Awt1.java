3b9fae22a413569053338be4efb5c4319e26741f
Gongme <k102kg12@naver.com>
2023-04-14 11:38:15 +0900

    master

--- java2/src/java2/Awt1.java
+++ java2/src/java2/Awt1.java
@@ -1,74 +1,74 @@
-package java2;
-
-import java.awt.*;
-import java.awt.event.*;
-
-public class Awt1 {
-	// java > awt, swing (GUI)
-	// awt 생성방식
-	// Frame > panel > object 생성
-//	적용방식 : Object > panel > Frame 적용
-
-	public static void main(String[] args) {
-
-		Frame fr = new Frame(); // 프레임 생성
-		closepm cp = new closepm();
-		fr.setTitle("사용자 로그인"); // 창 타이틀
-		fr.setSize(500, 500); // 창 사이즈
-		fr.setVisible(true); // 사용 유/무
-		fr.addWindowListener(cp); // 창닫기
-		Panel p = new Panel(); // 패널 생성
-
-//		오브젝트 생성
-		Checkbox c1 = new Checkbox("Auto Login");
-		TextField id = new TextField(10);
-		Button btn = new Button("LOGIN");
-		Button btn2 = new Button("CLOSE");
-		Button btn3 = new Button("AUTO ID");
-
-		btn.setBackground(Color.blue);
-		btn.setBounds(160, 40, 130, 30);
-//		add: 추가 
-		p.add(id); // 패널에 오브젝트 추가
-		p.add(btn);
-		p.add(c1);
-		p.add(btn2);
-		p.add(btn3);
-
-		// 프레임에 패널을 추가
-		fr.add(p);
-
-		// addActionListener : 버튼 클릭시 핸들링 메소드
-		btn3.addActionListener(new ActionListener() {
-			String userid = "hong";
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// 이벤트 발생
-				id.setText(userid); // id이름을 가진 오브젝트 안에 값을 적용
-
-			}
-		});
-		// 닫기 버튼
-		btn2.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-
-			}
-		});
-	}
-
-}
-
-class closepm extends WindowAdapter {
-	@Override
-	public void windowClosing(WindowEvent e) {
-
-		super.windowClosing(e);
-		System.out.println("프로그램 종료");
-		System.exit(0);
-	}
-
-}
+//package java2;
+//
+//import java.awt.*;
+//import java.awt.event.*;
+//
+//public class Awt1 {
+//	// java > awt, swing (GUI)
+//	// awt 생성방식
+//	// Frame > panel > object 생성
+////	적용방식 : Object > panel > Frame 적용
+//
+//	public static void main(String[] args) {
+//
+//		Frame fr = new Frame(); // 프레임 생성
+//		closepm cp = new closepm();
+//		fr.setTitle("사용자 로그인"); // 창 타이틀
+//		fr.setSize(500, 500); // 창 사이즈
+//		fr.setVisible(true); // 사용 유/무
+//		fr.addWindowListener(cp); // 창닫기
+//		Panel p = new Panel(); // 패널 생성
+//
+////		오브젝트 생성
+//		Checkbox c1 = new Checkbox("Auto Login");
+//		TextField id = new TextField(10);
+//		Button btn = new Button("LOGIN");
+//		Button btn2 = new Button("CLOSE");
+//		Button btn3 = new Button("AUTO ID");
+//
+//		btn.setBackground(Color.blue);
+//		btn.setBounds(160, 40, 130, 30);
+////		add: 추가 
+//		p.add(id); // 패널에 오브젝트 추가
+//		p.add(btn);
+//		p.add(c1);
+//		p.add(btn2);
+//		p.add(btn3);
+//
+//		// 프레임에 패널을 추가
+//		fr.add(p);
+//
+//		// addActionListener : 버튼 클릭시 핸들링 메소드
+//		btn3.addActionListener(new ActionListener() {
+//			String userid = "hong";
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// 이벤트 발생
+//				id.setText(userid); // id이름을 가진 오브젝트 안에 값을 적용
+//
+//			}
+//		});
+//		// 닫기 버튼
+//		btn2.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				System.exit(0);
+//
+//			}
+//		});
+//	}
+//
+//}
+//
+//class closepm extends WindowAdapter {
+//	@Override
+//	public void windowClosing(WindowEvent e) {
+//
+//		super.windowClosing(e);
+//		System.out.println("프로그램 종료");
+//		System.exit(0);
+//	}
+//
+//}
--- java2/src/project1/Array1.java
+++ java2/src/project1/Array1.java
@@ -1,11 +1,40 @@
 package project1;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array1 {
 
+	//ArrayList 배열 문자,숫자 혼합 사용하기
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("테스트");
+		
+		String data[]= {"hong","park"}; // 문자 배열
+		int data2[] = {10,20}; // 숫자 배열
+		
+		//ArrayList 가 String 자료형으로 int 배열은 받지 못함
+		
+		//Object 문자,숫자 자료형과 관련없이 모든 자료형을 받아주는 자료형 구조
+		ArrayList<Object> li = new ArrayList<>(Arrays.asList(data));
+		li.add(data2[0]);
+		li.add(data2[1]);
+		System.out.println(li);
+		
+		//계산방식
+		//1.
+		//int sum = (int)li.get(2) + (int)li.get(3);
+		
+		//2.
+//		Object sum = (int)li.get(2) + (int)li.get(3);
+		
+		//3.
+		Object sum = (Integer)li.get(2) + (Integer)li.get(3);
+		System.out.println(sum);
 
+		
+		//추가로 ArrayList 안에 데이터가 어떤 자료형인지 알아내는 방법.
+		//.getClass().getName() 를 이용하여 해당 데이터가 문자인지 숫자인지 파악
+		System.out.println(li.get(2).getClass().getName());
 	}
 
 }
